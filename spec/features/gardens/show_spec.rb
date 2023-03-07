b80d0e54fb335c9d54c9a22878600b503116a5c2
Jade Stewart <jadekstewart3@gmail.com>
2023-03-07 10:23:35 -0700

    Refine tests to make sure they are robust

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -9,22 +9,23 @@ RSpec.describe 'Garden Show Page' do
       @tomato = Plant.create!(name: "Tomato", description: "Red", days_to_harvest: 100)
       @basil = Plant.create!(name: "Basil", description: "Green", days_to_harvest: 50)
       @peppers = Plant.create!(name: "Jalapenos", description: "Also Green", days_to_harvest: 50)
-      @peppers_1 = Plant.create!(name: "Jalapenos", description: "Also Green", days_to_harvest: 50)
+
       PlantPlot.create!(plot_id: @plot_1.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_1.id, plant_id: @basil.id)
       PlantPlot.create!(plot_id: @plot_1.id, plant_id: @basil.id)
       PlantPlot.create!(plot_id: @plot_2.id, plant_id: @basil.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_1.id, plant_id: @peppers.id)
       PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers_1.id)
       visit garden_path(@garden)
     end
     context 'When I visit a garden show page' do
       it "I see a list of plants that are in that garden's plots and that are unique, and take less than 100 days to harvest" do
         expect(page).to have_content(@garden.name)
+        expect(page).to have_content("Turing Community Garden")
         expect(page).to have_content("Unique Plants in Garden:")
         within "#unique_plants" do
-          expect(page).to have_content("Basil")
+          expect(page).to have_content("Basil", count: 1)
           expect(page).to have_content("Jalapenos", count: 1)
           expect(page).to_not have_content("Tomato")
         end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -13,20 +13,20 @@ RSpec.describe Garden do
       @tomato = Plant.create!(name: "Tomato", description: "Red", days_to_harvest: 100)
       @basil = Plant.create!(name: "Basil", description: "Green", days_to_harvest: 50)
       @peppers = Plant.create!(name: "Jalapenos", description: "Also Green", days_to_harvest: 50)
-      @peppers_1 = Plant.create!(name: "Jalapenos", description: "Also Green", days_to_harvest: 50)
 
       PlantPlot.create!(plot_id: @plot_1.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_1.id, plant_id: @basil.id)
       PlantPlot.create!(plot_id: @plot_1.id, plant_id: @basil.id)
       PlantPlot.create!(plot_id: @plot_2.id, plant_id: @basil.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @tomato.id)
+      PlantPlot.create!(plot_id: @plot_1.id, plant_id: @peppers.id)
+      PlantPlot.create!(plot_id: @plot_1.id, plant_id: @peppers.id)
       PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers.id)
-      PlantPlot.create!(plot_id: @plot_2.id, plant_id: @peppers_1.id)
     end
 
     it 'returns unique plants that take less than 100 days to harvest' do
       expect(@garden.unique_plants_less_than_100_days).to eq([@basil.name, @peppers.name])
-      expect(@garden.unique_plants_less_than_100_days).to_not eq([@tomato.name, @basil.name, @peppers.name, @peppers_1.name])
+      expect(@garden.unique_plants_less_than_100_days).to_not eq([@tomato.name, @basil.name, @peppers.name])
     end
   end
 end
